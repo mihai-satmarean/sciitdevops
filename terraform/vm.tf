777324ce6ccf34fb47fe578507f694520da267bf
Darius Petricele <darius.petricele@sitline.ro>
2024-11-13 17:21:59 +0200

    commit

---
+++ terraform/vm.tf
@@ -0,0 +1,16 @@
+# There are currently no configuration options for the provider itself.
+
+resource "virtualbox_vm" "node" {
+  count = 1
+  name  = format("node-%02d", count.index + 1)
+  #image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  image  = "/DevOps-ScoalaInformala/sciitdevops/terraform/bionic-server-cloudimg-amd64-vagrant.box"
+  cpus   = 1
+  memory = "512 mib"
+  # user_data = file("${path.module}/user_data")
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "vboxnet1"
+  }
+}
