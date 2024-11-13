8e5383fb7be2bfa56bb80d9ef7c9262bbdbc2aee
ionmariana531 <ionmariana531@yahoo.com>
2024-11-13 17:57:59 +0000

    Remove image location #6

--- main.tf
+++ main.tf
@@ -12,8 +12,7 @@ terraform {
 resource "virtualbox_vm" "node" {
   count     = 2
   name      = format("node-%02d", count.index + 1)
-  image     = "/Users/marius.buricel/terraform/devops/virtualbox.box"
-#  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 2
   memory    = "512 mib"
   user_data = file("${path.module}/user_data")
