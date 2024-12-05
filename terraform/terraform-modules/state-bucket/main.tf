2c694905a6602ef24883361635ed0492eb7a3f6c
mihai-satmarean <4729542+mihai-satmarean@users.noreply.github.com>
2024-12-05 11:27:21 +0100

    Update main.tf
    improved handling

--- terraform/terraform-modules/state-bucket/main.tf
+++ terraform/terraform-modules/state-bucket/main.tf
@@ -1,16 +1,42 @@
+# main.tf
 provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "your-terraform-state-bucket"
-  acl    = "private"
-
-  versioning {
-    enabled = true
+# Local backend for initial setup
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
   }
+}
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket        = "terraform-state-${terraform.workspace}"
+  acl           = "private"
+  force_destroy = true # For simplicity in training scenarios; remove in production.
 
   tags = {
     Name = "Terraform State Bucket"
   }
+}
+
+resource "aws_s3_bucket_versioning" "state_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Terraform backend configuration post-initialization
+output "backend_config" {
+  value = <<EOT
+  bucket = "${aws_s3_bucket.terraform_state_bucket.bucket}"
+  key    = "state/${terraform.workspace}/terraform.tfstate"
+  region = "${var.region}"
+  EOT
+}
+
+variable "region" {
+  default = "eu-central-1"
 }
